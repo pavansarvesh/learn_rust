e977e268f1f0bc31dd20f9c7bc7cab5118b4b21c
pavansarvesh <pavansarvesh31@gmail.com>
2025-11-27 16:13:41 +0000

    code: 01 start of repo

---
+++ 01_hello_rust/examples/hello.rs
@@ -0,0 +1,9 @@
+// Attribute - metadata of compiler
+#![allow(unused)] //allows unused variables
+
+fn main() {
+    // main() - entry point of rust programe
+    // Macros in Rust generate code at compile time and are invoked with an exclamation mark (!)
+    // println! is a macro that prints text to the console
+    println!("Hello World")
+}
